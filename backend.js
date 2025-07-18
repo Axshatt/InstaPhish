98774060fd73ac5bc23dfb7ffba5357a7f036f96
Akshat <you@example.com>
2025-07-18 23:59:59 +0530

    fixes

--- backend.js
+++ backend.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const path = require('path');
+const cors = require('cors')
 const {CredModal} = require("./db")
 
 const app = express()
 app.use(express.static('public'))
+app.use(cors())
 app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
